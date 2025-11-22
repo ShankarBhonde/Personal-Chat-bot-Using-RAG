4f264070013edefe7d77af9cdc27ce4ab66687ec
Shankar Bhonde <96866856+ShankarBhonde@users.noreply.github.com>
2025-11-22 19:42:58 +0530

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -18,7 +18,7 @@ st.markdown("""
 <style>
 #sticky-blink {
     position: fixed;
-    top: 0%;                     /* middle vertically */
+    top: 5%;                     /* middle vertically */
     left: 50%;                    /* middle horizontally */
     transform: translate(-50%, -50%);
     z-index: 9999;
