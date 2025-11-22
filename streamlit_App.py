5d412af705cc18999917dddba90e5aaf3848868b
Shankar Bhonde <96866856+ShankarBhonde@users.noreply.github.com>
2025-11-22 19:40:12 +0530

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -16,29 +16,27 @@ st.set_page_config(page_title="Document Genie", layout="wide")
 st.markdown("""
 
 <style>
-/* Sticky box */
 #sticky-blink {
     position: fixed;
-    top: 0;                         /* show at very top */
+    top: 0;                 
     left: 50%;
     transform: translateX(-50%);
     z-index: 9999;
-    padding: 10px 0;
+    padding: 8px 20px;
     width: 100%;
     text-align: center;
-    background: rgba(0, 0, 0, 0.7); /* optional background */
+    background: rgba(0, 0, 0, 0.65);
 }
 
-/* Blinking animation text */
 .blink {
-  font-size: 35px;
-  font-weight: bold;
-  color: #00C4FF;
-  animation: blinker 1s linear infinite;
+    font-size: 35px;
+    font-weight: bold;
+    color: #00C4FF;
+    animation: blinker 1s linear infinite;
 }
 
 @keyframes blinker {
-  50% { opacity: 0; }
+    50% { opacity: 0; }
 }
 </style>
 
